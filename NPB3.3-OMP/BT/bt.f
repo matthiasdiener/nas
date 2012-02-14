d1b7c06ce133de81bc3203312eb8cad911f9220c
Matthias Diener <mdiener@turing.inf.ufrgs.br>
2012-02-14 16:49:52 -0200

    started remapping feature

--- NPB3.3-OMP/BT/bt.f
+++ NPB3.3-OMP/BT/bt.f
@@ -151,6 +151,7 @@ c---------------------------------------------------------------------
 
           if (mod(step, 20) .eq. 0 .or. 
      >        step .eq. 1) then
+     		 call remap(step)
              write(*, 200) step
  200         format(' Time step ', i4)
           endif
---
+++ NPB3.3-OMP/BT/remap.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+void remap_(int* step) {
+	
+	printf("xxx %d", *step);
+	//setaffinity()
+	
+}
+
+
+
+
+static void initialize_dynmap_() {
+
+	//int []
+
+}
+
+
+/*extern gomp_resolve_num_threads (int);*/
+
+
+/*void GOMP_parallel_start( void (*fn) (void *), void *data, unsigned num_threads) {*/
+
+/*	printf("libgomp was here\n"); */
+/*	int * ptr =0;*/
+/*	*ptr = 42;*/
+/*	//num_threads = gomp_resolve_num_threads (num_threads);*/
+/*    //gomp_team_start (fn, data, num_threads, 0);*/
+/*    //load_libraray(libgomp.so)*/
+/*}*/
+
