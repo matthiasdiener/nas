4626943c49c226a573172a087e8f486596c6d38b
Eduardo <ehmcruz@ehmcruz-vmware.(none)>
2012-03-31 20:55:25 -0300

    v

--- NPB3.3-OMP/remap.c
+++ NPB3.3-OMP/remap.c
@@ -3,6 +3,7 @@
 #include <assert.h>
 
 #include <map_algorithm.h>
+#include <mapping-lib.h>
 
 /*
 	types:
@@ -12,8 +13,10 @@
 		3: barrier
 */
 
-static uint32_t *current_map;
+static uint32_t *current_map, *new_map;
 static uint16_t nthreads;
+
+//static thread_mapping_t map;
 
 static void check_init()
 {
@@ -23,10 +26,45 @@ static void check_init()
 		assert(wrapper_dynmap_initiaized());
 		
 		nthreads = wrapper_get_threads();
+
 		current_map = (uint32_t*)calloc(nthreads, sizeof(uint32_t));
 		assert(current_map != NULL);
+		
+		new_map = (uint32_t*)calloc(nthreads, sizeof(uint32_t));
+		assert(new_map != NULL);
+		
+		mapping_lib_copy_initial_map(current_map);
+
+		{ int i; for (i=0; i<nthreads; i++) { current_map[i] = 0; } }
+		//{ int i; printf("init with map: "); for (i=0; i<nthreads; i++) { printf("%i,", current_map[i]); } printf("\n"); }
 	}
 }
+
+#ifdef MAPPING_LIB_REMAP_SIMICS_COMM_PATTERN_REALMACHINESIDE
+	static void remap_check_migrate(thread_mapping_t *tm, int type)
+	{
+		static uint32_t n_migrations = 0;
+		uint32_t diff, i;
+		
+//		if (type != 0)
+//			return;
+
+		tm->map = new_map;
+		wrapper_generate_thread_mapping(tm);
+		
+		diff = wrapper_generate_difference_between_mappings(current_map, new_map, tm->nthreads);
+//		{ int i; printf("new map type %i: ", type); for (i=0; i<nthreads; i++) { printf("%i,", new_map[i]); } printf("\n diff %i\n\n", diff); } getchar();
+		if (diff > 0) {
+			n_migrations++;
+			printf("migrando %i diff %u\n", n_migrations, diff);
+
+//			{ int i; printf("new map type %i: ", type); for (i=0; i<nthreads; i++) { printf("%i,", new_map[i]); } printf("\n"); }
+		}
+		
+		for (i=0; i<nthreads; i++)
+			current_map[i] = new_map[i];
+	}
+#endif
 
 void remap_time_step(int step)
 {
@@ -45,10 +83,9 @@ void remap_time_step(int step)
 			tm = wrapper_get_comm_pattern(0, step);
 			assert(tm != NULL);
 			
-			//{int i, j; printf("comm matrix(type %i value %i)\n", 0, step); for (i=0; i<tm->nthreads; i++) { for (j=0; j<tm->nthreads; j++) { printf("  %llu", tm->comm_matrix[i][j]); } printf("\n"); } printf("\n"); }
+			remap_check_migrate(tm, 0);
 			
-			tm->map = current_map;
-			wrapper_generate_thread_mapping(tm);
+			//{int i, j; printf("comm matrix(type %i value %i)\n", 0, step); for (i=0; i<tm->nthreads; i++) { for (j=0; j<tm->nthreads; j++) { printf("  %llu", tm->comm_matrix[i][j]); } printf("\n"); } printf("\n"); }
 		}
 	#endif
 }
@@ -69,6 +106,18 @@ void *__wrap_GOMP_parallel_start(void *func, void *data, unsigned nt)
 	#ifdef MAPPING_LIB_REMAP_SIMICS_COMM_PATTERN_SIMSIDE
 		mapping_lib_remap(1, n);
 	#endif
+
+	#ifdef MAPPING_LIB_REMAP_SIMICS_COMM_PATTERN_REALMACHINESIDE
+		{
+			thread_mapping_t *tm;
+			
+			tm = wrapper_get_comm_pattern(1, n);
+			assert(tm != NULL);
+			
+			remap_check_migrate(tm, 1);
+		}
+	#endif
+
 	n++;
 	return __real_GOMP_parallel_start(func, data, nt);
 }
@@ -84,6 +133,18 @@ void *__wrap_GOMP_parallel_end()
 	#ifdef MAPPING_LIB_REMAP_SIMICS_COMM_PATTERN_SIMSIDE
 		mapping_lib_remap(2, n);
 	#endif
+
+	#ifdef MAPPING_LIB_REMAP_SIMICS_COMM_PATTERN_REALMACHINESIDE
+		{
+			thread_mapping_t *tm;
+			
+			tm = wrapper_get_comm_pattern(2, n);
+			assert(tm != NULL);
+			
+			remap_check_migrate(tm, 2);
+		}
+	#endif
+
 	n++;
 }
 
@@ -100,6 +161,18 @@ void *__wrap_GOMP_barrier()
 		#ifdef MAPPING_LIB_REMAP_SIMICS_COMM_PATTERN_SIMSIDE
 			mapping_lib_remap(3, n);
 		#endif
+		
+		#ifdef MAPPING_LIB_REMAP_SIMICS_COMM_PATTERN_REALMACHINESIDE
+			{
+				thread_mapping_t *tm;
+			
+				tm = wrapper_get_comm_pattern(3, n);
+				assert(tm != NULL);
+			
+				remap_check_migrate(tm, 3);
+			}
+		#endif
+		
 		n++;
 	}
 }
