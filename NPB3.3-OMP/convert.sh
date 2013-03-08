956f8a9e33ea86a0c33051d54a93942dec36a161
Matthias Diener <mdiener@inf.ufrgs.br>
2013-03-08 18:43:45 -0300

    build 16 threads automatically

--- NPB3.3-OMP/convert.sh
+++ NPB3.3-OMP/convert.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-if [ $# -lt 3 ]; then
-	echo "Usage: $0 <binary> <#threads> <class>"
+if [ $# -ne 2 ]; then
+	echo "Usage: $0 <binary> <class>"
 	exit 1
 fi
+
+echo "### Converting source code and compiling it"
 
 LIST=$(nm $1 | awk '{print $3}' | grep "_$" | grep -v __ | grep -v  "^_" | grep -v timer_ | grep -v print_results_ | sort | uniq | sed s/.$//)
 
 PROG=$(basename $1 | sed s/\\..*//)
 DIR=$(basename $1)
-NTHREADS=$2
-CLASS=$3
-
+NTHREADS=15
+CLASS=$2
 
 for i in $(seq 0 $NTHREADS); do
 	echo ${i}
@@ -24,14 +25,18 @@ for i in $(seq 0 $NTHREADS); do
 		(cd $DIRID; find . -name "*.[hf]" -or -name "*.incl" | xargs sed -i s/\\b$sym\\b/${sym}_${i}/g;)
 		(cd $DIRID; find . -name "*.f" | xargs sed -i s/program\\\s.*/subroutine\ ${PROG}$i/;)
 	done
-	(cd $DIRID; make CLASS=$CLASS)
+	(cd $DIRID; make clean; make CLASS=$CLASS link)
 done
 
 echo "#define BENCHMARK $PROG" > ../app.h
-echo "#define NTHREADS $NTHREADS" >> ../app.h
 
-gcc -O2 -fopenmp -o ../${PROG}_${CLASS}_$NTHREADS ../${DIR}_*/*.o ../common/print_results.o ../common/timers.o ../common/wtime.o ../starter_pt.c -lgfortran -Wall -pthread
 
+echo "### Linking Final Binary"
+CMD="gcc -o ../${PROG}_${CLASS}_$NTHREADS -O2 -fopenmp ../${DIR}_*/*.o ../common/print_results.o ../common/timers.o ../common/wtime.o ../starter_pt.c -lgfortran -Wall -pthread"
+
+echo $CMD
+
+$CMD
 
 for i in $(seq 0 $NTHREADS); do
 	DIRID=../${DIR}_$i
@@ -39,3 +44,6 @@ for i in $(seq 0 $NTHREADS); do
 done
 
 rm ../app.h
+
+
+echo "### Done"
--- NPB3.3-OMP/starter_pt.c
+++ NPB3.3-OMP/starter_pt.c
@@ -5,34 +5,20 @@
 
 #include "app.h"
 
-#define PASTE2(f,b) f ## b
-#define EVAL2(f,b) PASTE2(f,b)
-#define PASTE3(f,m,b) f ## m ## b
-#define EVAL3(f,m,b) PASTE3(f,m,b)
-#define BM(i) EVAL3(BENCHMARK,i,_)
-#define str(s) #s
-#define xstr(s) str(s)
-#define xx(i) case i: printf("%s\n", xstr(BM(i))); extern void BM(i)(void); BM(i)(); break;
-#define CA xx(__COUNTER__)
+#define PASTE(f,b) f ## b ## _
+#define EVAL(f,b) PASTE(f,b)
+#define BM(i) EVAL(BENCHMARK,i)
+#define STR(s) #s
+#define XSTR(s) STR(s)
+#define CASEDEF(i) case i: printf("Starting %s\n", XSTR(BM(i))); extern void BM(i)(void); BM(i)(); break;
+#define CA CASEDEF(__COUNTER__)
 
-#define X0 CA
-#define X1 CA X0
-#define X2 CA X1
-#define X3 CA X2
-#define X4 CA X3
-#define X5 CA X4
-#define X6 CA X5
-#define X7 CA X6
-#define X8 CA X7
-
-#define FC EVAL2(X,NTHREADS)
-#define FUNC_CALLS FC
 
 void* call(void *info) {
 	long id = (long) info;
 
 	switch(id) {
-		FUNC_CALLS
+		CA CA CA CA CA CA CA CA CA CA CA CA CA CA CA CA 
 	}
 
 	return NULL;
@@ -42,12 +28,19 @@ int main(int argc, char *argv[]) {
 	pthread_t *threads;
 	long i;
 
-	threads = malloc(NTHREADS+1 * sizeof(pthread_t));
+	if (argc<2) {
+		printf("Usage: %s <#threads>\n", argv[0]);
+		exit(1);
+	}
 
-	for (i=0; i<NTHREADS+1; i++)
+	int nt = atoi(argv[1]);
+
+	threads = malloc(nt * sizeof(pthread_t));
+
+	for (i=0; i<nt; i++)
 		pthread_create(&threads[i], NULL, &call, (void*) i);
 
-	for (i=0; i<NTHREADS+1; i++)
+	for (i=0; i<nt; i++)
 		pthread_join(threads[i], NULL);
 
 	printf("Finished\n");
