dd3e1bc7b4478b4a566c49b0eeeb76ffaf2435b2
Matthias Diener <mdiener@inf.ufrgs.br>
2013-03-12 15:17:16 -0300

    use x as delimiter

--- NPB3.3-OMP/convert.sh
+++ NPB3.3-OMP/convert.sh
@@ -7,7 +7,8 @@ fi
 
 echo "### Compiling original source code and finding symbols"
 
-make CLASS=W
+make clean
+make CLASS=A
 
 BLACKLIST="print_results|timer|storage" # Can not be empty
 LIST=$(nm *.o | awk '{print $3}' | grep "_$" | grep -v __ | grep -v  "^_" | sort | uniq | sed s/.$// | egrep -v "\b($BLACKLIST)\b" )
@@ -20,14 +21,14 @@ CLASS=$1
 echo "### Converting source code and compiling it"
 
 for i in $(seq 0 $NTHREADS); do
-	echo ${i}
+	echo Compiler iteration: ${i}
 	DIRID=../${DIR}_$i
 	rm -rf $DIRID
 	mkdir $DIRID
 	cp * $DIRID 2>/dev/null
 	for sym in $LIST; do
 		echo -e \\t $sym;
-		(cd $DIRID; find . -name "*.[hf]" -or -name "*.incl" | xargs sed -i "/include/!s/\b$sym\b/${sym}_${i}/g";)
+		(cd $DIRID; find . -name "*.[hf]" -or -name "*.incl" | xargs sed -i "/include/!s/\b$sym\b/${sym}x${i}/g";)
 	done
 	(cd $DIRID; find . -name "*.f" | xargs sed -i s/program\\\s.*/subroutine\ ${DIR}$i/;)
 	(cd $DIRID; make clean; make CLASS=$CLASS link)
