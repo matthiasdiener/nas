ea91d60d5be5b849485b17700e47ed2a5f84532e
Matthias Diener <mdiener@inf.ufrgs.br>
2013-04-14 15:03:55 -0300

    add mappings

--- run.sh
+++ run.sh
@@ -1,17 +1,60 @@
 #!/bin/bash
 
-if [ $# -ne 5 ]; then
-	echo "Usage: $0 <version> <#threads> <#runs> <size> <benchmarks>"
-	exit 1
+usage() {
+	echo "Usage: $0 <version> <#threads> <#runs> <size> <benchmarks> <mapping>"
+	echo "Available Mappings: round-robin (RR), random static (RS), operating system (OS)"
+	exit $1
+}
+
+if [ $# -ne 6 ]; then
+	usage 1
 fi
 
+trap 'exit 3' SIGINT
 
-VER=$1
+VER=${1^^}
 THREADS=$2
 RUNS=$3
-SIZE=$4
-BM=$5
+SIZE=${4^^}
+BM=${5,,}
+MAP_ALGO=${6^^}
 
+PUS=$(cat /proc/cpuinfo | grep processor | tail -1 | awk '{print $3}')
+if [ $(($THREADS-1)) -gt $PUS ]; then
+	echo "Number of threads larger than number of processors, cannot continue"
+	usage 4
+fi
+
+do_map() {
+	MAP="-binding user:"
+	case ${MAP_ALGO} in
+		"RR") # round robin
+			for i in $(seq 0 $(($THREADS-1))); do
+				MAP="$MAP$i,"
+			done
+			MAP=${MAP:0:${#MAP} - 1} ;;
+
+		"RS") # random static
+			unset mapped
+			mapped=()
+			RANDOM=$1
+			for j in $(seq 0 $(($THREADS-1))); do
+				while [ 1 ]; do
+					cpu=$(($RANDOM%$(($PUS+1))))
+					if [ "${mapped[$cpu]}" != "x" ]; then break; fi
+				done
+				mapped[$cpu]="x"
+				MAP="$MAP$cpu,"
+			done
+			MAP=${MAP:0:${#MAP} - 1} ;;
+		"OS") # OS mapping
+			MAP=""
+			;;
+		*)
+			echo "Illegal Mapping '${MAP_ALGO}'"
+			usage 2
+	esac
+}
 
 DIR="NPB3.3-$VER/bin"
 
@@ -20,8 +63,9 @@ OUTPATH=results/$VER/$THREADS/$SIZE
 for bm in $BM; do
 	mkdir -p $OUTPATH/$bm
 	for i in $(seq 1 $RUNS); do
+		do_map $i
 		name=$OUTPATH/$bm/$(date|tr ' ' '-').txt
-		echo $i
-		mpirun -np $THREADS $DIR/$bm.$SIZE.$THREADS | tee $name
+		# echo $i
+		echo mpirun $MAP -np $THREADS $DIR/$bm.$SIZE.$THREADS | tee $name
 	done
 done
