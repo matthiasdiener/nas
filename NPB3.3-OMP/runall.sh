d9c743ff9481793722d129093c90f1b5dcac6297
Matthias Diener <mdiener@inf.ufrgs.br>
2016-08-24 16:06:08 -0300

    bugfixes runall

--- NPB3.3-OMP/runall.sh
+++ NPB3.3-OMP/runall.sh
@@ -10,17 +10,19 @@ bm="bt cg ep ft is lu mg sp ua"
 OUTDIR=~/Dropbox/results/$(hostname)/$(basename $DIR)/
 mkdir -p $OUTDIR
 
+# does not work with quotation marks:
 exe() { echo "++ $@" |& tee -a $OUTDIR/$b.txt ;  if [[ "$1" == "export" ]]; then $@ ; fi ;  $@ |& tee -a $OUTDIR/$b.txt ; }
 
 for b in $bm; do
-	echo -n > $OUTDIR / $b.txt #clean output file
+	echo -n > $OUTDIR/$b.txt #clean output file
 	exe echo "running $b"
 	exe date
-	exe uname - a
+	exe uname -a
 	[[ -x /usr/bin/hwloc-ls ]] && exe hwloc-ls
 
-	exe export GOMP_CPU_AFFINITY = 0 - 1024 KMP_AFFINITY=explicit,verbose,proclist=[0 - 1024] 
-	exe pwd exe git log --oneline --no-color -1 
+	exe export GOMP_CPU_AFFINITY=0-227 KMP_AFFINITY=explicit,verbose,proclist=[0-227]
+	exe pwd
+	exe git log -1 --oneline
 	exe sudo -E perf stat -A -a -e instructions,cache-misses,cache-references,cycles bin/$b.B
 done
 
